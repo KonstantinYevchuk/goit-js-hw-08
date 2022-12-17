2fdf7326dd8c655be1ba32bd8b31e3484e619b85
KonstantinYev <109310447+KonstantinYevchuk@users.noreply.github.com>
2022-12-17 21:01:09 +0200

    update task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+import simpleLightbox from 'simplelightbox';
 // Change code below this line
 
 console.log(galleryItems);
+console.log(simpleLightbox);
+
+const galleryContainer = document.querySelector(".gallery")
+
+const array = galleryItems.reduce(
+    (acc, {preview, original, description}) => {
+       return acc + 
+       `<a class="gallery__item" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+     </a>`
+    }, '');
+
+   
+galleryContainer.insertAdjacentHTML('beforeend', array)
+var lightbox = new SimpleLightbox('.gallery a', { 
+  captionsData: "alt",
+  captionPosotion: 'bottom',
+  captionDaley: 250
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
+console.log(Player);
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+
+function onPlay(options) {
+    options = {
+         duration: 61.857,
+         percent: 0.049,
+         seconds: 3.034
+         }   
+ }
+player.on('timeupdate', onPlay);
+
+
+
+player.setCurrentTime(30.456).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
 
