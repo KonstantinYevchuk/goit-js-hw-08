b0a09350bf55c0949841d19a97aa619c0710671e
KonstantinYev <109310447+KonstantinYevchuk@users.noreply.github.com>
2022-12-19 11:11:51 +0200

    Update Task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,31 +1,16 @@
 import Player from '@vimeo/player';
 console.log(Player);
-const iframe = document.querySelector('#vimeo-player');
+var throttle = require('lodash.throttle');
 
+const KEY = "videoplayer-current-time";
+const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
-function onPlay(options) {
-    options = {
-         duration: 61.857,
-         percent: 0.049,
-         seconds: 3.034
-         }   
- }
-player.on('timeupdate', onPlay);
+ 
+player.on('timeupdate', throttle(data => {
+    localStorage.setItem(KEY, JSON.stringify(data.seconds))}, 1000) 
+);
 
+player.setCurrentTime(localStorage.getItem(KEY) || 0);
 
-
-player.setCurrentTime(30.456).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
-});
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,25 @@
+var throttle = require('lodash.throttle');
 
+const formEl = document.querySelector(".feedback-form");
+const emailEl = document.querySelector("input")
+const massegeEl = document.querySelector("textarea")
+const button = document.querySelector("button")
+
+formEl.addEventListener("input", inputCallBack)
+// emailEl.addEventListener("input", inputCallBack);
+// massegeEl.addEventListener("input", inputCallBack);
+// button.addEventListener("submit", buttonCallBack);
+
+function inputCallBack(evt) { 
+        const { elements: {email, message} 
+    } = evt.currentTarget;
+        
+        console.log(email.value)
+        console.log(message.value);
+        
+        localStorage.setItem("feedback-form-state", JSON.stringify(evt.currentTarget.email, evt.currentTarget.message))  
+}
+
+// function buttonCallBack(evt) {
+//     evt.preventDefault()
+// }
